418ac2da3f16e955833e91d9f1a518208c5adb59
GENTOO\cypri <cypriane81@gmail.com>
2025-12-06 17:14:24 +0000

    Progress

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,4 +1,4 @@
-extends Node3D
+class_name Player extends Node3D
 
 var usingL = false
 var usingR = false
@@ -6,16 +6,69 @@ var usingR = false
 var blockingL = false
 var blockingR = false
 
-var punchSpeed = 1.5
+var punchSpeed = 2.5
+var punchDamage = 10
 
 var armLDefPos: Vector3
 var armRDefPos: Vector3
 
+var stamMax: float = 100
+var stamCur: float = 100
+
+var healthMax: float = 100
+var healthCur: float = 100
+
+var punchCost: float = 20
+var blockCost: float = 10
+
+var recovery: float = 20
+var recoverycd: float = 0
+
+var cam: Camera3D
+var camForward: Vector3
+var camDefPos: Vector3
+var camBob: float
+var camBobTween: Tween
+
+var hitRecoil: float
+
+@onready var stamBar: ProgressBar = %StamBar
+@onready var dmgBorder: TextureRect = %DMGBorder
+
+static var ins: Player
+
+func _init():
+    ins = self
+
 func _ready():
+    cam = $Cam
+    camForward = cam.rotation
+    camDefPos = cam.position
     armLDefPos = $ArmL.position
     armRDefPos = $ArmR.position
 
+    camBobTween = create_tween().set_loops()
+    camBobTween.tween_property(self, "camBob", 1.0, 0.4).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+    camBobTween.tween_property(self, "camBob", 0.0, 0.4).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+    camBobTween.tween_property(self, "camBob", -1.0, 0.4).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+    camBobTween.tween_property(self, "camBob", 0.0, 0.4).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+
+func CamStuff(_dt: float, mousePos: Vector2):
+    var lookOffset = Vector2(mousePos.x / get_viewport().size.x, mousePos.y / get_viewport().size.y)
+    lookOffset -= Vector2(.5, .5)
+    cam.rotation.y = camForward.y - (lookOffset.x * 0.3)
+    cam.rotation.x = camForward.x - (lookOffset.y * 0.3)
+
+    cam.position.y = camDefPos.y + (camBob * 0.01)
+    cam.position.z = camDefPos.z + hitRecoil
+
 func _process(_dt):
+    recoverycd -= _dt
+    if recoverycd < 0:
+        stamCur += recovery * _dt
+        if stamCur > stamMax:
+            stamCur = stamMax
+
     var mousePos: Vector2 = get_viewport().get_mouse_position()
     var left: bool = mousePos.x < get_viewport().size.x / 2
     if left:
@@ -29,42 +82,75 @@ func _process(_dt):
         if !usingL:
             $ArmL.position = armLDefPos
         
-    if Input.is_action_just_pressed("punch") || Input.is_action_just_pressed("block"):
-        if left:
-            if !usingL:
-                usingL = true
-                if Input.is_action_just_pressed("punch"):
-                    $ArmL.position.z += -1
-                    var punchTween: Tween = create_tween()
-                    punchTween.tween_interval(1/punchSpeed)
-                    punchTween.tween_callback(Callable(self, "ResetArmL"))
-                else:
-                    blockingL = true
-                    $ArmL.position += Vector3(0.3, 0.5, 0)
-                    var blockTween: Tween = create_tween()
-                    blockTween.tween_interval(1/punchSpeed)
-                    blockTween.tween_callback(Callable(self, "ResetArmL"))
-        else:
-            if !usingR:
-                usingR = true
-                if Input.is_action_just_pressed("punch"):
-                    $ArmR.position.z += -1
-                    var punchTween: Tween = create_tween()
-                    punchTween.tween_interval(1/punchSpeed)
-                    punchTween.tween_callback(Callable(self, "ResetArmR"))
-                else:
-                    blockingR = true
-                    $ArmR.position += Vector3(-0.3, 0.5, 0)
-                    var blockTween: Tween = create_tween()
-                    blockTween.tween_interval(1/punchSpeed)
-                    blockTween.tween_callback(Callable(self, "ResetArmR"))
+    if left && !usingL:
+        if Input.is_action_just_pressed("punch") && stamCur > punchCost:
+            var punchTween: Tween = create_tween()
+            punchTween.tween_property($ArmL, "position:z", $ArmL.position.z - 1, 1/punchSpeed).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+            punchTween.tween_callback(Callable(self, "ResetArmL"))
+            stamCur -= punchCost
+            recoverycd = 0.25
+            usingL = true
+        elif Input.is_action_just_pressed("block") && stamCur > blockCost:
+            blockingL = true
+            $ArmL.position += Vector3(0.3, 0.5, 0)
+            var blockTween: Tween = create_tween()
+            blockTween.tween_interval(1/punchSpeed)
+            blockTween.tween_callback(Callable(self, "ResetArmL"))
+            stamCur -= blockCost
+            recoverycd = 0.25
+            usingL = true
+    elif !left && !usingR:
+        if Input.is_action_just_pressed("punch") && stamCur > punchCost:
+            var punchTween: Tween = create_tween()
+            punchTween.tween_property($ArmR, "position:z", $ArmR.position.z - 1, 1/punchSpeed).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+            punchTween.tween_callback(Callable(self, "ResetArmR"))
+            stamCur -= punchCost
+            recoverycd = 0.25
+            usingR = true
+        elif Input.is_action_just_pressed("block") && stamCur > blockCost:
+            blockingR = true
+            $ArmR.position += Vector3(-0.3, 0.5, 0)
+            var blockTween: Tween = create_tween()
+            blockTween.tween_interval(1/punchSpeed)
+            blockTween.tween_callback(Callable(self, "ResetArmR"))
+            stamCur -= blockCost
+            recoverycd = 0.25
+            usingR = true
+
+    if stamCur < 0:
+        stamCur = 0
+
+    stamBar.value = stamCur
+
+    CamStuff(_dt, mousePos)
 
 func ResetArmL():
-    $ArmL.position = armLDefPos
-    usingL = false
-    blockingL = false
+    if !blockingL:
+        Enemy.ins.TakeDamage(punchDamage)
+    var resetTween: Tween = create_tween()
+    resetTween.tween_property($ArmL, "position", armLDefPos, 0.1)
+    resetTween.tween_callback(func(): usingL = false)
+    resetTween.tween_callback(func(): blockingL = false)
 
 func ResetArmR():
-    $ArmR.position = armRDefPos
-    usingR = false
-    blockingR = false
+    if !blockingR:
+        Enemy.ins.TakeDamage(punchDamage)
+    var resetTween: Tween = create_tween()
+    resetTween.tween_property($ArmR, "position", armRDefPos, 0.1)
+    resetTween.tween_callback(func(): usingR = false)
+    resetTween.tween_callback(func(): blockingR = false)
+
+func TakeDamage(val: float):
+    healthCur -= val
+    dmgBorder.modulate.a = 1.0
+    var dmgFlash: Tween = create_tween()
+    dmgFlash.tween_property(dmgBorder, "modulate:a", 0.0, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+    if healthCur < 0:
+        Die()
+
+    hitRecoil = 0.05
+    var hitRecoilTween: Tween = create_tween()
+    hitRecoilTween.tween_property(self, "hitRecoil", 0.0, 0.25).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+
+func Die():
+    pass
