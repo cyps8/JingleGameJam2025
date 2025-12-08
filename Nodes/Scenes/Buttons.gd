85d1081a23f11f7c35334eb8d869d59ee88b1ea3
cypri <cypriane81@gmail.com>
2025-12-08 18:00:34 +0000

    UI Improvements

--- Nodes/Scenes/Buttons.gd
+++ Nodes/Scenes/Buttons.gd
@@ -3,8 +3,13 @@ extends Button
 @export var hoverScale: Vector2 = Vector2(1.1,1.1)
 @export var PressedScale: Vector2 = Vector2(0.9,0.9)
 
+@export var disableForWeb: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	focus_mode = FOCUS_NONE
+	if (OS.get_name() == "Web" && disableForWeb):
+		disabled = true
 	mouse_entered.connect(_buttonEnter)
 	mouse_exited.connect(_buttonExit)
 	focus_exited.connect(_buttonExit)
@@ -16,6 +21,7 @@ func _init_pivot() -> void:
 	
 func _buttonEnter() -> void:
 	create_tween().tween_property(self,"scale",hoverScale,0.1).set_trans(Tween.TRANS_SINE) 
+	SFXPlayer.ins.PlaySound(7, SFXPlayer.SoundType.SFX, 1.0, (randf() * 0.4) + 0.8)
  
 func _buttonExit() -> void:
 	create_tween().tween_property(self,"scale",Vector2.ONE,0.1).set_trans(Tween.TRANS_SINE) 
@@ -23,4 +29,5 @@ func _buttonExit() -> void:
 func _buttonPressed() -> void:
 	var buttonPressedTween: Tween = create_tween()
 	buttonPressedTween.tween_property(self,"scale",PressedScale,0.06).set_trans(Tween.TRANS_SINE) 
-	buttonPressedTween.tween_property(self,"scale",hoverScale,0.12).set_trans(Tween.TRANS_SINE) 
+	buttonPressedTween.tween_property(self,"scale",hoverScale,0.12).set_trans(Tween.TRANS_SINE)
+	SFXPlayer.ins.PlaySound(6, SFXPlayer.SoundType.SFX, 1.0, (randf() * 0.4) + 0.8)
